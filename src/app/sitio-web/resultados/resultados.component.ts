1d95434db43cea1bdb1d9e0674418024be4ccee0
Sergio Chamorro <sergiochamo96@gmail.com>
2021-12-13 03:28:31 -0300

    Se agrego detalle de partido en resultados de home

--- src/app/sitio-web/resultados/resultados.component.ts
+++ src/app/sitio-web/resultados/resultados.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PartidoDTO } from 'src/app/models/modelsCommon';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { JugadorPartidoDTO, PartidoDTO } from 'src/app/models/modelsCommon';
+import { JugadorService } from 'src/app/services/jugador.service';
 import { PartidoService } from 'src/app/services/partido.service';
 
 @Component({
@@ -10,9 +12,23 @@ import { PartidoService } from 'src/app/services/partido.service';
 export class ResultadosComponent implements OnInit {
 
   partidosJugados: PartidoDTO[] = [];
+  partidoSeleccionado: PartidoDTO = {};
+  dia: string | undefined;
+  hora: string | undefined;
+  equipo1: string | undefined;
+  equipo2: string | undefined;
+  golesLocal: number | undefined;
+  golesVisitante: number | undefined;
+  fechaTorneo: string | undefined;
+
+  // lista de jugadores
+  jugadoresLocal: JugadorPartidoDTO[] = [];
+  jugadoresVisitante: JugadorPartidoDTO[] = [];
 
   constructor(
+    private modalService: NgbModal,
     private _partidoService: PartidoService,
+    private _jugadorService: JugadorService
   ) { }
 
   ngOnInit(): void {
@@ -30,6 +46,29 @@ export class ResultadosComponent implements OnInit {
         console.log('Ocurrió un error al obtener los partidos disputados', 'Error');
       }
     );
+  }
+
+  async mostrarDetalle(modalDetalle: any, par: PartidoDTO) {
+    this.equipo1 = par.equipoLocal;
+    this.equipo2 = par.equipoVisitante;
+    this.fechaTorneo = par.fechaTorneo;
+    this.dia = par.dia;
+    this.hora = par.hora;
+    this.golesLocal = par.golesLocal;
+    this.golesVisitante = par.golesVisitante;
+    this.partidoSeleccionado = par;
+
+    let jugLocal = await this._jugadorService.getJugadoresPartidoDisputado(par.idEquipoLocal, par.idPartido).toPromise();
+    if (jugLocal != null)
+      this.jugadoresLocal = jugLocal.data;
+      this.jugadoresLocal = this.jugadoresLocal.filter(x => x.goles != undefined && x.goles > 0);
+
+    let jugVisitante = await this._jugadorService.getJugadoresPartidoDisputado(par.idEquipoVisitante, par.idPartido).toPromise();
+    if (jugVisitante != null)
+      this.jugadoresVisitante = jugVisitante.data;
+      this.jugadoresVisitante = this.jugadoresVisitante.filter(x => x.goles != undefined && x.goles > 0);
+
+    this.modalService.open(modalDetalle, { size: 'lg' });
   }
 
 }
